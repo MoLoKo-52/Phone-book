a91ba902e639b963ad834cdd8be34135fb8c929e
mc_lavrushka <mc.lavrushka@gmail.com>
2025-04-30 18:50:54 +0300

    feat: add client interface

---
+++ client/lib/data/functions/open_url.dart
@@ -0,0 +1,8 @@
+import 'package:url_launcher/url_launcher.dart' show launchUrl;
+
+Future<void> openUrl(String urlString) async {
+  final Uri url = Uri.parse(urlString);
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch $url');
+  }
+}
---
+++ client/lib/data/functions/verify_signature.dart
@@ -0,0 +1,16 @@
+import 'dart:convert' show utf8;
+import 'package:crypto/crypto.dart' show Hmac, sha256;
+
+bool verifySignature(
+  String name,
+  String number,
+  bool found,
+  String signatureFromServer,
+) {
+  const secret = 'supersecretkey';
+  final message = '$name:$number:${found ? 'True' : 'False'}'; //
+  final key = utf8.encode(secret);
+  final hmacSha256 = Hmac(sha256, key);
+  final digest = hmacSha256.convert(utf8.encode(message)).toString();
+  return digest == signatureFromServer;
+}
